5ffd42944c0479ac55e0cf908315911bdae4c8a6
unknown <guptapriyanshu973@gmail.com>
2021-05-03 18:08:23 +0530

    Updated Readme and Learnings

--- Day 3/index.js
+++ Day 3/index.js
@@ -7,6 +7,70 @@
 
 // Arrays in Javascript
 
-var name = ['Priyanshu', 'Rounak', 'Avnish', 'Krish', 'Bantai Rapper', 'Dj Ramesh', 'Dj Shashi'];
+// var name = ['Priyanshu', 'Rounak', 'Avnish', 'Krish', 'Bantai Rapper', 'Dj Ramesh', 'Dj Shashi'];
 
-console.log(name[6])
+// console.log(name.length)
+
+// for loop in array
+
+// var myFriends = ['Priyanshu', 'Rounak', 'Avnish', 'Krish', 'Amitesh'];
+
+// for (var i = 0; i < myFriends.length; i++) {
+//     console.log(myFriends[i])
+// }
+
+// for in loop ---> ES6 Feature
+
+// var myFriends = ['Priyanshu', 'Rounak', 'Avnish', 'Krish', 'Amitesh'];
+
+// for (let elements in myFriends) {
+//     console.log(myFriends[elements]);
+// }
+
+// var myFriends = ['Priyanshu', 'Rounak', 'Avnish', 'Krish', 'Amitesh'];
+
+// for(let elements of myFriends) {
+//     console.log(elements);
+// }
+
+// for each loop
+
+// var myFriends = ['Priyanshu', 'Rounak', 'Avnish', 'Krish', 'Amitesh'];
+
+// myFriends.forEach((elements, index, array) => {
+//     console.log(`${elements}`)
+     // console.log(`Index : ${index}`)
+     // console.log(`array : ${array}`)
+// })
+
+// Array Subsections 2 --> Searching and Filter
+
+// var myFriends = ['Priyanshu', 'Rounak', 'Avnish', 'Krish', 'Amitesh'];
+
+// console.log(myFriends.indexOf('Priyanshu'))
+
+// Sorting of Array
+
+// var fruits = ["Banana", "Orange", "Apple", "Mango"];
+// console.log(fruits.sort());
+
+// CURD Operation in JavaScript
+
+// var animals = ['Dog', 'Cat', 'Pig', 'Elephant', 'Lion'];
+// animals.push('Tiger', 'Cow', 'Goat', 'Rinosorous', 'Horse')
+// console.log(animals)
+
+// Using unshift method to push the elements in front
+
+// var animals = ['Dog', 'Cat', 'Pig', 'Elephant', 'Lion'];
+// animals.unshift('Tiger', 'Cow', 'Goat', 'Rinosorous', 'Horse')
+// console.log(animals)
+
+// 2nd Example
+
+// const number = [1, 2, 3, 4, 5, 6, 7]
+// number.unshift(-7, -6, -5, -4, -3, -2, -1, 0)
+// console.log(number)
+
+
+
--- README.md
+++ README.md
@@ -17,5 +17,19 @@
     2. Modern JavaScript
          1. var, let, const
 
+[Resources](https://github.com/codewithpriyansh/100-Days-Of-Code/blob/main/Day%202/resources.txt)
+
+## Day 3
+1. Learned
+    1. JavaScript
+        1. Fat Arrow Function
+        2. For in Loop
+        3. For of Loop
+        4. For Each Loop
+        5. Arrays in JavaScript
+        6. Searching and filter of Arrays
+        7. Sorting the Arrays
+        8. CURD Operations in Arrays
+
 
 [Resources](https://github.com/codewithpriyansh/100-Days-Of-Code/blob/main/Day%202/resources.txt)
