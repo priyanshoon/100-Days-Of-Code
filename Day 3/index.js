c3a29b030b208b5cdd3be6d8fc14af88348dc225
unknown <guptapriyanshu973@gmail.com>
2021-05-03 10:31:49 +0530

    Day 3 modern javascript

---
+++ Day 3/index.js
@@ -0,0 +1,12 @@
+// Fat Arror Function in ES6
+
+// const sum = (a, b) => {
+//     return a + b
+// }
+// console.log(sum(1,2));
+
+// Arrays in Javascript
+
+var name = ['Priyanshu', 'Rounak', 'Avnish', 'Krish', 'Bantai Rapper', 'Dj Ramesh', 'Dj Shashi'];
+
+console.log(name[6])
