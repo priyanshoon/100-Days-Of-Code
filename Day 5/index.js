84b70e72d65eace4a7525759f610cdd8e293ce66
unknown <guptapriyanshu973@gmail.com>
2021-05-05 21:34:31 +0530

    Day 5

---
+++ Day 5/index.js
@@ -0,0 +1,28 @@
+// How to set Date in JavaScript
+
+// var currDate = new Date();
+
+// console.log(currDate.setDate(5));
+
+// Time Get methods
+
+// let currDate = new Date();
+
+// console.log(currDate.getTime())
+// console.log(currDate.getHours())
+// console.log(currDate.getMinutes())
+// console.log(currDate.getSeconds())
+
+// Time Set Method
+
+// let currDate = new Date();
+
+// // console.log(currDate.setTime())
+// console.log(currDate.setHours(19))
+// console.log(currDate.setMinutes(7))
+// console.log(currDate.setSeconds(6))
+// console.log(currDate.setMilliseconds(344))
+
+
+// Math Objects in JavaScript
+
