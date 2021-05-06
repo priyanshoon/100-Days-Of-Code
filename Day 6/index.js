a9defffc115e16a909e6dae2846cef5d31c74cfa
unknown <guptapriyanshu973@gmail.com>
2021-05-06 19:26:54 +0530

    Day 6

---
+++ Day 6/index.js
@@ -0,0 +1,8 @@
+// DOM In JavaScript
+
+// Windows Vs Documents
+
+// Window is the main container or we can say the global object and any operation related to entire browser window can be a part of window object
+
+// Document is the child of window object 
+
