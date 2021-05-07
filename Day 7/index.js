390d8115f999b6316b3fc994bc7f2385bdc2d2ad
unknown <guptapriyanshu973@gmail.com>
2021-05-07 15:15:50 +0530

    Day 7

---
+++ Day 7/index.js
@@ -0,0 +1,46 @@
+// OOPs in JavaScript
+// (Object Oriented Programmings)
+
+// 1st Way
+
+// let bioData = {
+//     myName: "Priyanshu",
+//     myAge: 17,
+//     getData: function(){
+//         console.log(`My name is ${bioData.myName} and my age is ${bioData.myAge}`)
+//     }
+// }
+
+// bioData.getData()
+
+// 2nd Way
+
+// let bioData = {
+//     myName: "Priyanshu",
+//     myAge: 17,
+//     getData() {
+//         console.log(`My name is ${bioData.myName} and my age is ${bioData.myAge}`)
+//     }
+// }
+
+// bioData.getData()
+
+// Object in Object
+
+// let bioData = {
+//     myName: {
+//         myRealName: "Priyanshu",
+//         myNickName: "PriyanshGupt"
+//     },
+//     myAge: 17,
+//     getData() {
+//         console.log(`My real name is ${bioData.myName.myRealName} and my nick name is ${bioData.myName.myNickName} and my age is ${bioData.myAge}`)
+//     }
+// }
+
+// bioData.getData()
+
+// this in JavaScript
+
+// console.log(this)
+
