9bb6af3c4294957ad79a6bd61b2a91acaf6f3f9c
Priyanshu Gupta <guptapriyanshu973@gmail.com>
2021-05-11 19:10:23 +0530

    New learnings

--- Day 11/tut8.cpp
+++ Day 11/tut8.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 
 int main() {
+	
 	// Loops in cpp
 
 	// for(int i = 0; i < 10; i++){
---
+++ Day 11/tut9.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "This is tutorial 9";
+	return 0;
+}
