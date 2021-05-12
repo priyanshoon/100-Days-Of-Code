229102d971d4b1da809ecbd793fa1dd4ae8bf27d
Priyanshu Gupta <guptapriyanshu973@gmail.com>
2021-05-12 06:35:22 +0530

    Day 12

--- Day 12/tut1.cpp
+++ Day 12/tut1.cpp
@@ -1,7 +1,45 @@
 #include <iostream>
 using namespace std;
 
+// C++ Strings
+
+// "" ---> For String
+// '' ---> For Chatacter
+
 int main() {
-	cout << "Hello, World!" << endl;
+
+	// string str = "Hello, World!\n";
+	// Concatination in string
+
+	// string str2 = "Hello, Priyanshu!";
+	// string result = str + str2;
+	// cout << result << endl;
+
+	// string str = "Hello";
+	// for(int i = 0; i < str.size(); i++) {
+	// 	cout << str[i] << endl;
+	// }
+
+	// getline function
+
+	// int t;
+	// cin >> t;
+	// cin.ignore();
+	// while(t--) {
+	// 	string s;
+	// 	getline(cin, s);
+	// 	cout << s << endl;
+	// }
+
+	// Reversing the string
+	
+	string str;
+	cin >> str;
+	string strReverse;
+	for(int i = str.size() -1; i >= 0; --i) {
+		strReverse.push_back(str[i]);
+	} 
+	cout << strReverse << endl;
+
 	return 0;
 }
